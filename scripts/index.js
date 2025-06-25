b9d6a079badd2c5aaac5c1521ea8ac935e79a8b2
Carlos Collado <collado.carlos96@gmail.com>
2025-06-24 23:30:12 -0600

    Feat: Agrega 6 tarjetas predeterminadas desde Js

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,50 @@ let closeButton = pagePopup.querySelector(".popup__close");
 let nameInput = pagePopup.querySelector("#input-nombre");
 let jobInput = pagePopup.querySelector("#input-aboutme");
 
+let elementsContainer = container.querySelector(".content__elements");
+
+function addCard(name, link) {
+  const cardsTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardsTemplate
+    .querySelector(".elements__card")
+    .cloneNode(true);
+  cardElement.querySelector(".elements__image").setAttribute("src", link);
+  cardElement.querySelector(".elements__image").setAttribute("alt", name);
+  cardElement.querySelector(".elements__text").textContent = name;
+  elementsContainer.append(cardElement);
+}
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+initialCards.forEach(({ name, link }) => {
+  addCard(name, link);
+});
+
 function openPopup() {
   pagePopup.classList.add("page__popup_popup_opened");
   nameInput.value = profileName.textContent;
