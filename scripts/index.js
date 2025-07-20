e5945d31ae87f9f018c29b88c24517e911e79f07
Carlos Collado <collado.carlos96@gmail.com>
2025-07-19 20:09:01 -0600

    Feat: Agrega validaciones de formulario y manejo de eventos en ventanas emergentes

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,17 @@ const addButton = profile.querySelector(".profile__add-button");
 
 const elementsContainer = container.querySelector(".content__elements");
 
+import { resetFormValidation } from "./validate.js";
+import { hasInvalidInput } from "./validate.js";
+
+const validationConfig = {
+  inputSelector: ".form__input",
+  submitButtonSelector: ".form__button",
+  inactiveButtonClass: "form__button_inactive",
+  inputErrorClass: "form__input_type_error",
+  errorClass: "form__input-error_active",
+};
+
 /* 
 Función que añade nuevas tarjetas al grid,
 ya sea al incio o al final dependiendo del parametro especificado.
@@ -81,81 +92,126 @@ initialCards.forEach(({ name, link }) => {
   createCard(name, link);
 });
 
-//Funciones popup
-
-function openPopup(popupElement) {
-  popupElement.classList.add("page__popup_popup_opened");
-}
-
-function closePopup(popupElement) {
-  popupElement.classList.remove("page__popup_popup_opened");
-}
-
 // popup Editar perfil
 
 const popupEditProfile = document.querySelector("#popup-editprofile");
-const closeEditProfile = popupEditProfile.querySelector(".popup__close");
 
-const nameInput = popupEditProfile.querySelector("#input-nombre");
-const jobInput = popupEditProfile.querySelector("#input-aboutme");
+const nameInput = popupEditProfile.querySelector("#name-input");
+const jobInput = popupEditProfile.querySelector("#aboutme-input");
 
-editButton.addEventListener("click", () => {
+const handleEditForm = () => {
   openPopup(popupEditProfile);
   nameInput.value = profileName.textContent;
   jobInput.value = profileOccupation.textContent;
-});
+};
 
-closeEditProfile.addEventListener("click", () => closePopup(popupEditProfile));
-
-const profileForm = popupEditProfile.querySelector(".popup__form");
-
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileOccupation.textContent = jobInput.value;
-  closePopup(popupEditProfile);
-}
-profileForm.addEventListener("submit", handleProfileFormSubmit);
+editButton.addEventListener("click", handleEditForm);
 
 //popup Nuevo lugar
 
 const popupNewPlace = document.querySelector("#popup-newplace");
-const closeNewPlace = popupNewPlace.querySelector(".popup__close");
+const titleInput = popupNewPlace.querySelector("#title-input");
+const linkInput = popupNewPlace.querySelector("#link-input");
 
-const titleInput = popupNewPlace.querySelector("#input-titulo");
-const linkInput = popupNewPlace.querySelector("#input-enlace");
+const handleNewPlaceForm = () => {
+  openPopup(popupNewPlace);
+};
 
-addButton.addEventListener("click", () => openPopup(popupNewPlace));
-closeNewPlace.addEventListener("click", () => closePopup(popupNewPlace));
-
-const newPlaceForm = popupNewPlace.querySelector(".popup__form");
-
-function handleNewPlaceFormSubmit(evt) {
-  evt.preventDefault();
-  createCard(titleInput.value, linkInput.value, "prepend");
-  titleInput.value = "";
-  linkInput.value = "";
-  closePopup(popupNewPlace);
-}
-
-newPlaceForm.addEventListener("submit", handleNewPlaceFormSubmit);
+addButton.addEventListener("click", handleNewPlaceForm);
 
 // Popup vista de imagen
 const popupViewImage = document.querySelector("#popup-viewimage");
-const closeViewImage = popupViewImage.querySelector(".popup__close");
-
-// Función que permite abrir y cerrar la visualización de una imagen
+const popupImage = popupViewImage.querySelector(".popup__view-image");
+const popupName = popupViewImage.querySelector(".popup__view-title");
+// Función que permite abrir la visualización de una imagen
 function viewImage(link, title) {
-  const popupImage = popupViewImage.querySelector(".popup__view-image");
-  const popupName = popupViewImage.querySelector(".popup__view-title");
   popupImage.setAttribute("src", link);
   popupImage.setAttribute("alt", title);
   popupName.textContent = title;
+}
 
-  closeViewImage.addEventListener("click", () => {
-    closePopup(popupViewImage);
-    popupImage.setAttribute("src", "");
-    popupImage.setAttribute("alt", "");
-    popupName.textContent = "";
-  });
+const resetViewElement = () => {
+  popupImage.setAttribute("src", "");
+  popupImage.setAttribute("alt", "");
+  popupName.textContent = "";
+};
+
+//Funciones Popup
+
+const handlePopupClose = (popupElement) => {
+  return function (evt) {
+    const isEscape = evt.key === "Escape";
+    const isOverlayClick = evt.target.classList.contains("page__popup");
+    const isButtonClick = evt.target.classList.contains("popup__close");
+    if (isEscape || isOverlayClick || isButtonClick) {
+      if (popupElement.id === "popup-viewimage") {
+        resetViewElement();
+      }
+      closePopup(popupElement);
+    }
+  };
+};
+
+const handleFormSubmit = (popupElement) => {
+  return function (evt) {
+    evt.preventDefault();
+    const inputList = Array.from(popupElement.querySelectorAll(".form__input"));
+
+    if (!hasInvalidInput(inputList)) {
+      switch (popupElement.id) {
+        case "popup-editprofile":
+          console.log(popupElement.id);
+          profileName.textContent = nameInput.value;
+          profileOccupation.textContent = jobInput.value;
+          break;
+        case "popup-newplace":
+          console.log(popupElement.id);
+          createCard(titleInput.value, linkInput.value, "prepend");
+          break;
+        default:
+          break;
+      }
+      closePopup(popupElement);
+    }
+  };
+};
+
+const togglePopupListeners = (popupElement, action) => {
+  if (!popupElement.popupCloseListener) {
+    popupElement.popupCloseListener = handlePopupClose(popupElement);
+  }
+  const formElement = popupElement.querySelector(".popup__form");
+  if (!popupElement.setFormSubmit && formElement) {
+    popupElement.setFormSubmit = handleFormSubmit(popupElement);
+  }
+
+  const listener = popupElement.popupCloseListener;
+  const setSubmit = popupElement.setFormSubmit;
+  const popupCloseElement = popupElement.querySelector(".popup__close");
+
+  if (action === "add") {
+    popupCloseElement.addEventListener("click", listener);
+    document.addEventListener("keydown", listener);
+    popupElement.addEventListener("mousedown", listener);
+    formElement ? formElement.addEventListener("submit", setSubmit) : "";
+  } else if (action === "remove") {
+    popupCloseElement.removeEventListener("click", listener);
+    document.removeEventListener("keydown", listener);
+    popupElement.removeEventListener("mousedown", listener);
+    formElement ? formElement.removeEventListener("submit", setSubmit) : "";
+  }
+};
+
+function openPopup(popupElement) {
+  popupElement.classList.add("page__popup_popup_opened");
+  togglePopupListeners(popupElement, "add");
+}
+
+function closePopup(popupElement) {
+  const form = popupElement.querySelector("form");
+  if (form) {
+    resetFormValidation(form, validationConfig);
+  }
+  togglePopupListeners(popupElement, "remove");
+  popupElement.classList.remove("page__popup_popup_opened");
 }
